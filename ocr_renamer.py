1537bb934a33f9757b1a86c81cde2cc9da405e24
Joel Leow <48181140+jwpleow@users.noreply.github.com>
2019-08-31 23:49:19 +0100

    Using a single proper split for spaces, replacing O's with 0's

--- ocr_renamer.py
+++ ocr_renamer.py
@@ -2,6 +2,7 @@ from PIL import Image
 import pytesseract
 import os
 import rawpy
+import re
 
 ### Modify the variables here ###
 FileExtension = ".CR2"
@@ -10,18 +11,28 @@ FileExtension = ".CR2"
 ### String parameters that must be fulfilled
 def check_string(string):
     if len(string) != 15: #check if string length is correct
+        # print("wrong string length",flush=True)
         return 0
+    if 'O' in string[7:15]: #replace any O's with 0's
+        string = string[0:7] + string[7:15].replace('O','0')
+        # print("replacing O with 0",flush=True)
+        # print("new string: " + string, flush=True)
     if not string[7:15].isdigit(): #check if last 8 characters are digit
+        # print("last few characters are not digits",flush=True)
         return 0
     if not string[0:3].isalpha(): #check if first 3 characters are alphabets
+        # print("first 3 chars are not alphabets",flush=True)
         return 0
     if string[3] != '_':
+        print("wrong string length",flush=True)
         return 0
     if not string[4:7].isalpha(): #check if 4-6th digits are alphabets
+        # print("5-7th characters are not alphabets",flush=True)
         return 0
+        # if there is a O in 7:15 replace with 0?
     return 1
 
-
+unrenamedfiles = [] #list to store unrenamed files
 directory = (os.getcwd())
 for filename in sorted(os.listdir(directory)): #iterate over every file
     if filename.endswith(FileExtension): #check for the extension of the file
@@ -30,15 +41,16 @@ for filename in sorted(os.listdir(directory)): #iterate over every file
             rgb = raw_image.postprocess()
         img = Image.fromarray(rgb)
         change = 0
-        for basewidth in range(600,2801,100): # Try a few different image resizes until OCR detects a long enough string
+        for basewidth in range(600,2201,100): # Try a few different image resizes until OCR detects a long enough string
             ## Code to resize the image while keeping aspect ratio - so that OCR can be more accurate
             wpercent = (basewidth/float(img.size[0]))
             hsize = int((float(img.size[1])*float(wpercent)))
             img = img.resize((basewidth,hsize), Image.ANTIALIAS)
             ## End resize
-            imgtextlist=pytesseract.image_to_string(img,config='--psm 6 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ_0123456789').split('\n') #Get data output and split into list
-            longesttext=max((max(imgtextlist, key=len)).split(' '), key=len) #Grab the longest string and split any whitespaces
-            # print(str(basewidth) + " result is: " + str(imgtextlist))
+            imgtext=pytesseract.image_to_string(img,config='--psm 6 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ_0123456789') #Get data output and split into list
+            # print(str(basewidth) + " width text read is: \n" + str(imgtext),flush=True)
+            longesttext=max(re.split(u'\s', imgtext),key=len) #split by space/tab/newline character
+            # print("Longest text is: ", longesttext,flush=True)
             if check_string(longesttext): #If longer than 13 characters, and no O or o, accept the string and use to rename file
                 if not (longesttext + ' A' + FileExtension) in os.listdir(directory):
                     os.rename(filename, longesttext + ' A' + FileExtension)
@@ -52,4 +64,6 @@ for filename in sorted(os.listdir(directory)): #iterate over every file
                 change = 1
                 break
         if change == 0:
+            unrenamedfiles.append(filename)
             print("Unable to find the text for file: " + filename, flush=True)
+
