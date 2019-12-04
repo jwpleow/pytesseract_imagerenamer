ed5014c4101bd77081e85c3ea30f509df320c7f3
Joel Leow <woeltic@gmail.com>
2019-12-04 23:15:46 +0000

    implemented multi-scale template matching, use better tesseract PSM, removed finer process fn

--- ocr_renamer.py
+++ ocr_renamer.py
@@ -1,3 +1,9 @@
+"""
+This script renames the images in the current working directory by their label (using Tesseract OCR) and D/V tag (using template matching)
+
+Multiscale template matching taken from https://www.pyimagesearch.com/2015/01/26/multi-scale-template-matching-using-python-opencv/
+"""
+
 from PIL import Image
 import pytesseract
 import os
@@ -10,7 +16,7 @@ from multiprocessing import Lock, Pool, Manager, Value
 
 ### Modify the variables here ###
 FileExtension = ".CR2" # File extension of the image
-threshold = 0.9 # threshold for template match to be accepted
+threshold = 0.8 # threshold for template match to be accepted
 
 # setup templates
 template_D = cv2.imread('Templates/D.png', 0) # flag 0 for grayscale image
@@ -20,9 +26,8 @@ template_V = cv2.imread('Templates/V.png', 0)
 if template_V is None:
 	raise FileNotFoundError("Templates/V.png could not be found")
 
-
 def filter_text(imgtext):
-    """"
+    """
     This function takes in the block of text from Tesseract and tries to find the correct string.
     Returns the string if found, else returns None 
     """
@@ -43,6 +48,7 @@ def rename_img(filename, text, dorsal_ventral):
     global FileExtension
     global renamed_file_counter
     renamed_file_counter.value += 1
+    lock.acquire()
     """" This function renames the image to its label + D/V/A + <number if filename is taken> """
     if not (f"{text} {dorsal_ventral}{FileExtension}") in os.listdir(os.getcwd()): #check if new file name already exists
         os.rename(filename, f"{text} {dorsal_ventral}{FileExtension}")
@@ -53,6 +59,7 @@ def rename_img(filename, text, dorsal_ventral):
             n += 1
         os.rename(filename, f"{text} {dorsal_ventral}({n}){FileExtension}")
         print(f"Renaming {filename} to {text} {dorsal_ventral}({n}){FileExtension}", flush=True)
+    lock.release()
     return
 
 def resize_img(img, new_width):
@@ -63,121 +70,104 @@ def resize_img(img, new_width):
 
 def match_template(img):
     """ This function tries to match the D or V template images with the img input, 
-    and returns either a 'D' or 'V' if either match, else returns 'A'
+    and returns the highest correlation coefficient along with the corresponding letter
     """
-    flag_D = False
-    flag_V = False
-    ret = ""
     global template_D
     global template_V
     res_D = cv2.matchTemplate(img, template_D, cv2.TM_CCOEFF_NORMED)
     res_V = cv2.matchTemplate(img, template_V, cv2.TM_CCOEFF_NORMED)
-    if np.amax(res_D) > threshold:
-        flag_D = True
-    if np.amax(res_V) > threshold:
-        flag_V = True
-
-    if flag_D == True and flag_V == True:
-        print("Both D and V detected!")
-        ret = "A"
-    elif flag_D == True:
-        ret = "D"
-    elif flag_V == True:
-        ret = "V"
+    val_D = np.amax(res_D)
+    val_V = np.amax(res_V)
+    if val_D > val_V:
+        return ['D', val_D]
     else:
-        # print("Neither D nor V was detected!")
-        ret = "A"
-    
-    return ret
+        return ['V', val_V]
 
 def process_image(filename):
-    # print(f"Looking at file: {filename}", flush=True)
-    with rawpy.imread(filename) as raw_image: #with so that the file is closed after reading
-        rgb = raw_image.postprocess() # return a numpy array
-    img = cv2.cvtColor(rgb, cv2.COLOR_RGB2BGR) # convert into openCV image
-    text_found = 0
-    for width in range(600,2201,100): # Try a few different image sizes until OCR detects a long enough string
+    
+    # Get openCV image from the raw image
+    with rawpy.imread(filename) as raw_image:
+        rgb = raw_image.postprocess()  # returns a numpy array
+    img = cv2.cvtColor(rgb, cv2.COLOR_RGB2BGR)  # convert into the correct colour for cv2
+
+    # initialise some variables to keep track of things found
+    renamed_flag = 0
+    label_found = None  # to hold the finalised label
+    labels_found = []  # stores the labels found (e.g. ZRC_ENT0004098), needs to have 2 similar hits before accepting
+    template_found = None  # to store the D/V string if threshold is reached
+    starting_width = 600
+    ending_width = 6000
+    """ Use only bottom half of image for label finding, and right side of image for template matching!!"""
+    # Try a few different image sizes until OCR detects a matching string
+    for width in range(starting_width, ending_width + 1, 100): 
+        text = None
         img_resized = resize_img(img, width)
-        # PyTesseract only seems to accept PIL image formats
-        imgtext=pytesseract.image_to_string(Image.fromarray(img_resized), config='--psm 6 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ_0123456789') #Get data output and split into list
-        text = filter_text(imgtext)
-        if text: 
-            text_found = 1
-            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # convert to grayscale for template matching
-            result = match_template(gray) # use openCV template to find if D or V template detected
-            lock.acquire()
-            rename_img(filename, text, result)
-            lock.release()
+        img_gray = cv2.cvtColor(img_resized, cv2.COLOR_BGR2GRAY)  # convert to grayscale for template matching
+
+        # do some OCR to find the label...
+        if not label_found:
+            # PyTesseract only seems to accept PIL image formats...
+            img_text=pytesseract.image_to_string(Image.fromarray(img_resized), config='--psm 3 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ_0123456789') #Get data output and split into list
+            text = filter_text(img_text)
+            if text:
+                labels_found.append(text)
+
+        # find the template only for every other width
+        if not template_found and width % 200 == 0:        
+            template_result = match_template(img_gray) 
+            if template_result[1] > threshold:
+                template_found = template_result[0]
+                # print(f"template found for {filename} at width {width} with coeff: {template_result[1]}",flush=True)
+
+        # set the label if it has been found twice
+        if text in labels_found and not label_found: # this occurs only if text is found twice
+            label_found = text
+            # print(f"label found for {filename} at width {width}", flush=True)
+
+        # at the end of the loop
+        if width == ending_width: 
+            if len(labels_found) == 1:
+                label_found = labels_found[0]
+            if label_found and not template_found:
+                rename_img(filename, label_found, 'A')
+                renamed_flag = 1
+                break
+
+        # rename and break loop if both have been found
+        if label_found and template_found:
+            rename_img(filename, label_found, template_found)
+            renamed_flag = 1
             break
-    if text_found == 0:
-        round_one_unrenamed_files.append(filename)
+
+    if renamed_flag == 0:
+        unrenamed_files.append(filename)
         # print(f"Unable to find the text for file: {filename}", flush=True)
 
-def process_image_finer(filename):
-    # print(f"Looking at file: {filename}", flush=True)
-    with rawpy.imread(filename) as raw_image: 
-        rgb = raw_image.postprocess() # return a numpy array
-    img = cv2.cvtColor(rgb, cv2.COLOR_RGB2BGR) # convert into openCV image
-    text_found = 0
-    for width in range(600,3051,50): # Try more and see if we get lucky?
-        if width % 100 != 0 or width > 2101: #only perform for sizes not already tried
-            img_resized = resize_img(img, width)
-            imgtext=pytesseract.image_to_string(Image.fromarray(img_resized), config='--psm 6 -c tessedit_char_whitelist=ABCDEFGHIJKLMNOPQRSTUVWXYZ_0123456789') #Get data output and split into list
-            text = filter_text(imgtext)
-            if text: 
-                text_found = 1
-                gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # convert to grayscale for template matching
-                result = match_template(gray) # use openCV template to find if D or V template detected
-                lock.acquire()
-                rename_img(filename, text, result)
-                lock.release()
-                break
-                
-        # ## Using a different config on pytesseract seems to cause no output suddenly?? (or was this already the case?) Find out why
-        # imgtext=pytesseract.image_to_string(img) #Get data output and split into list
-        # text = filter_text(imgtext)
-        # if text: 
-        #     rename_img(text)
-        #     text_found = 1
-        #     break
-    if text_found == 0:
-        failed_files.append(filename)
-        # print(f"Still unable to find the text for file: {filename}", flush=True)
-
-def init(l, _round_one_unrenamed_files, _failed_files, _renamed_file_counter): # initialise the global variables into the processor pool
+def init(l, _unrenamed_files, _renamed_file_counter): # initialise the global variables into the processor pool
     global lock
-    global round_one_unrenamed_files
+    global unrenamed_files
     global renamed_file_counter
-    global failed_files
     lock = l
-    round_one_unrenamed_files = _round_one_unrenamed_files
+    unrenamed_files = _unrenamed_files
     renamed_file_counter = _renamed_file_counter
-    failed_files = _failed_files
 
 if __name__ == '__main__':
     start = time.time()
     print("Looking at images...", flush=True)
     l = Lock()
     manager = Manager()
-    _round_one_unrenamed_files = manager.list() # holds shared list of unrenamed files during first round of processing
-    _failed_files = manager.list() # holds shared list of files that could not be renamed after all processing
+    _unrenamed_files = manager.list() # holds shared list of unrenamed files during first round of processing
     _renamed_file_counter = Value('i', 0)
     directory = os.getcwd()
-    files = [filename for filename in sorted(os.listdir(directory)) if filename.endswith(FileExtension) and not filename.startswith('ZRC_ENT')]
+    files = [filename for filename in sorted(os.listdir(directory)) if filename.endswith(FileExtension)] #  and not filename.startswith('ZRC_ENT')
     no_of_files = len(files)
 
-    pool = Pool(initializer=init, initargs=(l, _round_one_unrenamed_files, _failed_files, _renamed_file_counter))
+    pool = Pool(initializer=init, initargs=(l, _unrenamed_files, _renamed_file_counter))
     pool.map(process_image, files)
-
-    if _round_one_unrenamed_files:
-        print("Trying further steps on files that could not be renamed...", flush=True)
-        # print(_round_one_unrenamed_files, flush=True)
-        pool.map(process_image_finer, _round_one_unrenamed_files)
-
     pool.close()
     pool.join()
 
     print(f"The script took {(time.time() - start):.1f}s to rename {_renamed_file_counter.value} out of {no_of_files} files.")
-    if _failed_files:
+    if _unrenamed_files:
         print(f"The files that could not be renamed are:")
-        print(_failed_files)
+        print(_unrenamed_files)
